9f226026579fd4e713370865b4345d6962a68d21
PrabhathChathura <138102816+PrabhathChathura@users.noreply.github.com>
2024-04-21 23:47:19 +0530

    setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+import './header.css';
+
+import logo from '../../assets/images/eco-logo.png';
+import userIcon from '../../assets/images/user-icon.png';
+
+import { Container, Row } from "reactstrap";
+
+const Header = () => {
+  return (<header className="header">
+   <Container>
+  <Row>
+    <div className="nav__wrapper">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <div>
+          <h1>Green Treasure</h1>
+          <p>Since 2023</p>
+        </div>
+      </div>
+      
+      <div className="navigation">
+        <ul className="menu">
+          <li className="nav__item">
+            <NavLink to="home">Home</NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="shop">Shop</NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="cart">Cart</NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="nav__icons">
+
+        <span className="cart__icon">
+        <i class="ri-shopping-cart-2-line"></i>
+        </span>
+        <span>
+          <img src={userIcon} alt="" />
+        </span>
+      </div>
+
+      <div className="mobile__menu">
+        <span>
+          <i className="ri-menu-line"></i>
+        </span>
+      </div>
+    </div>
+  </Row>
+</Container>
+
+  </header> 
+  );
+  };
+
+export default Header;
+
