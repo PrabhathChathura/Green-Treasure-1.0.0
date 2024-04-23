9e53e928f0bc8b7978c0b58f67bf84830ba41d3c
PrabhathChathura <138102816+PrabhathChathura@users.noreply.github.com>
2024-04-24 00:40:18 +0530

    Refactor Helmet component and update Home page layout

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -5,6 +5,6 @@ const Helmet = (props) => {
     return(
         <div className="w-100"> {props.children} </div>
     )
-}
+};
 
 export default Helmet;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,47 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import {motion} from 'framer-motion';
+
 import Helmet from "../components/Helmet/Helmet";
 
 import { Container, Row, Col } from "reactstrap";
 
 import heroImg from '../assets/images/hero-img.png'
 
+import Services from "../services/Services";
+
+import "../styles/home.css";
+
 
 const Home = () => {
   const year = new Date().getFullYear()
   return (<Helmet title={'Home'}> 
-
-  <section className="hero_section">
+  <section className="hero__section">
     <Container>
       <Row>
-        <Col lg='6' md='6'>  
-        <div className="hero_content">
+        <Col lg="6" md="6">  
+        <div className="hero__content">
           <p className="hero__subtitle"> Trending product in {year}</p>
 
           <h2>Make Your Interior More Minamalist & Modern </h2>
 
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem 
+            ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor
+             sit amet, consectetur adipiscing elit</p>
 
-          <button className="buy__btn">SHOP NOW</button>
+          <motion.button whileTap={{ scale: 1.2 }} 
+          className="buy__btn"> 
+          <Link to="/shop">SHOP NOW</Link> 
+          </motion.button>
 
 
         </div>
 
       </Col>
 
-      <Col lg='6' md='6'>
+      <Col lg="6" md="6">
         <div className="hero__img">
           <img src={heroImg} alt="hero" />
         </div>  
@@ -37,9 +49,19 @@ const Home = () => {
       </Row>
     </Container>
 </section>
+
+<Services />
+
 </Helmet>
+
+
+
+
+
 );
   };
+
+
 
 export default Home;
 
