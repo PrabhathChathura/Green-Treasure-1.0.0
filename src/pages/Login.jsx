ad2e2a80b2d7ee16cad73f1d0a886ad15633303e
PrabhathChathura <138102816+PrabhathChathura@users.noreply.github.com>
2024-07-18 03:53:24 +0530

    total page
    first login entry

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Container,Row,Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
-  return <div>Login</div>;
+  return <Helmet title='Login'>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'className="m-auto text-center">
+          <h3 className="fw-bold fs-4">Login</h3>
+          
+          <Form className="auth__form">
+            <FormGroup className="form__group">
+              <input type = 'email' placeholder="Enter your email"/>
+
+            </FormGroup>
+          </Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+  </Helmet>
   };
 
 export default Login;
