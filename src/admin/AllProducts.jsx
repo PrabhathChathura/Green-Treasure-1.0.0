5c90d1c38bc9e17b94fa0b07818ee1455bdc3c4f
PrabhathChathura <138102816+PrabhathChathura@users.noreply.github.com>
2024-07-24 23:55:58 +0530

    all products
    admin dashboard

--- src/admin/AllProducts.jsx
+++ src/admin/AllProducts.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import productImg from '../assets/images/arm-chair-01.jpg';
+import useGetData from '../custom-hooks/useGetData';
 
 const AllProducts = () => {
-    return <div>products</div>;
+
+    const {data:productsData, loading} = useGetData('products');
+
+     
+    return ( 
+    
+    <section>
+        <Container>
+            <Row>
+                <Col lg= '12'>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Price</th>
+                            <th>
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                      
+                      {
+                        loading ? <h3>loading....</h3> :   productsData.map(item=>(
+                                <tr key = {item.id}>
+                                <td><img src={item.imgUrl} alt=""/></td>
+                                <td>{item.title}</td>
+                                <td>{item.category}</td>
+                                <td>${item.price}</td>
+                                <td> 
+                                    {}
+                                    <button className='btn btn-primary '>Delete</button>
+                                     </td>
+                            </tr>
+
+                            ))
+                        }
+                      
+                    
+                    </tbody>
+                </table>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+    )
 };
 
 export default AllProducts;
